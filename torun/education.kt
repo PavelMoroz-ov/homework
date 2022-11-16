fe252cc8b3d1860db6964c9a256318de8fef50e8
Ivan <i.s.komlik@gmail.com>
2022-11-16 16:03:30 +0300

    Update education.kt

--- torun/education.kt
+++ torun/education.kt
@@ -4,10 +4,10 @@ import com.example.myapplication.model.Animal2
 
 
 fun main (){
-    val animal1 = Animal1("Травоядные")
+    val animal1 = Animal1("Травоядные") // Надо создать два класса, с именем травоядные и хищники. Посмотрите правильные их перевод на английский
     val animal2 = Animal2("Плотоядные")
 
-    var ListOfCarnivores: List<String> = listOf("Тирг","Лев")
+    var ListOfCarnivores: List<String> = listOf("Тирг","Лев") // В списках должен быть тип Вашего класса List<Predator> - например, класс хищники
     var ListOfHerbivores: List<String> = listOf("Свинья","Лошадь")
     print(animal1.animal)
     println(ListOfHerbivores)
